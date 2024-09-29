49b58e0946cf611c8f3a0502f78ccb8839d36305
IlyaNikolaevitch <mister.bin1981@gmail.com>
2024-09-29 11:47:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-first = 35
-second = 75
-third = 35
-if first == second and first == third and second == third:
-    print(3)
-elif first == second or second == third or third == first:
-    print(2)
-else:
-    print(0)
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while  a < len(my_list):
+    if my_list[a] == 0:
+        a += 1
+        continue
+    if my_list[a] < 0:
+        break
+    print(my_list[a])
+    a += 1
